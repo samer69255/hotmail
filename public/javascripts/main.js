84b42711c39a4d558af5a1a92fd1af700ce18276
samer69255 <30310196+samer69255@users.noreply.github.com>
2018-11-09 22:28:19 +0300

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,7 +11,7 @@
     var emails2 = [];
     for (var i in emails) {
         var key = emails[i];
-       // if (key.indexOf("com.ar") > -1) continue;
+        if (key.indexOf("com.ar") > -1) continue;
           if (key.indexOf('@hotmail') > -1 || key.indexOf('@outlook') > -1 || key.indexOf('@live') > -1 )  emails2.push( key.replace(/\s/g, "" ) );
         }
     this.value = emails2.join('\n').trim();
